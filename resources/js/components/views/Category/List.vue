9065c9fd8840321c572bad15cbb4fef5e3d22df3
lokman_hosen <lokman.cse.pstu@gmail.com>
2021-08-28 21:30:23 +0600

    Working on project category list page

--- resources/js/components/views/Category/List.vue
+++ resources/js/components/views/Category/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 
-        <div id="people">
+        <div>
             <v-client-table :data="tableData" :columns="columns" :options="options"/>
         </div>
     </div>
@@ -12,20 +12,28 @@ import {ServerTable, ClientTable, Event} from 'vue-tables-2';
 
 export default {
     name: "List",
-    el: "#people",
-    data: {
-        columns: ['id', 'name', 'age'],
-        tableData: [
-            { id: 1, name: "John", age: "20" },
-            { id: 2, name: "Jane", age: "24" },
-            { id: 3, name: "Susan", age: "16" },
-            { id: 4, name: "Chris", age: "55" },
-            { id: 5, name: "Dan", age: "40" }
-        ],
-        options: {
-            // see the options API
+    data(){
+        return {
+            columns: ['id', 'name', 'age'],
+            tableData: [
+                { id: 1, name: "John", age: "20" },
+                { id: 2, name: "Jane", age: "24" },
+                { id: 3, name: "Susan", age: "16" },
+                { id: 4, name: "Chris", age: "55" },
+                { id: 5, name: "Dan", age: "40" }
+            ],
+            options: {
+                // see the options API
+                headings: {
+                    name: 'ID',
+                    code: 'Name',
+                    uri: 'Age'
+                },
+                /*sortable: ['name', 'code'],
+                filterable: ['name', 'code']*/
+            }
         }
-    }
+    },
 }
 
 
