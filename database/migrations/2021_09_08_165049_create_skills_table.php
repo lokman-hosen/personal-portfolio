10ba7f995f51cebb70c00953b970195db10d09a7
lokman_hosen <lokman.cse.pstu@gmail.com>
2021-09-08 23:37:23 +0600

    blog category factory continue

---
+++ database/migrations/2021_09_08_165049_create_skills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('difficulty', ['frontend', 'backend', 'server']);
+            $table->tinyInteger('status')->default(1)->comment('1=> active, 0=> inactive');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
